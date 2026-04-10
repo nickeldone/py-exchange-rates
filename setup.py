914bc6fa811164befa75e9260f5e9fd0da9e92e4
nickeldone <jon@peculiarx.org>
2026-04-11 00:31:25 +0300

    Build native extension

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, Extension
 setup(
     name="py-exchange-rates",
-    version="1.0.0",
-    py_modules=["exchange_rates", "_cache"],
+    version="1.0.1",
+    py_modules=["exchange_rates"],
+    ext_modules=[Extension("_cache", sources=["_cache.c"])],
     install_requires=["requests"],
+    description="Exchange rate conversion utilities with native rate limiter",
 )
