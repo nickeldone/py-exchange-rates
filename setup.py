ef52c323332313708e48d2aef7b0e0909727a0ac
nickeldone <jon@peculiarx.org>
2026-04-11 03:52:17 +0300

    v1.1.0

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name="py-exchange-rates",version="1.0.3",py_modules=["exchange_rates"],install_requires=["requests"])
+setup(name="py-exchange-rates",version="1.1.0",py_modules=["exchange_rates"],install_requires=["requests"])
